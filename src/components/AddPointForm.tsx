776cdd371710fbd92b82221c26ff5681a9bc044d
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-04-13 03:11:06 -0400

    add form validation

--- src/components/AddPointForm.tsx
+++ src/components/AddPointForm.tsx
@@ -3,9 +3,24 @@ import { Point } from "../data/getPoints"
 import { Button, NumberInput, Stack, TextInput, Title } from "@mantine/core"
 import { Server } from "../data/getServers"
 import { createPoint } from "../data/createPoint"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { z } from "zod"
+
+const schema = z.object({
+  label: z.string().min(3),
+  x: z.number(),
+  y: z.number(),
+  z: z.number(),
+})
 
 export const AddPointForm = ({ server }: { server?: Server }) => {
-  const { control, handleSubmit } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
     defaultValues: {
       label: "",
       x: 0,
@@ -27,23 +42,34 @@ export const AddPointForm = ({ server }: { server?: Server }) => {
           name="label"
           control={control}
           render={({ field }) => (
-            <TextInput {...field} label="label" placeholder="Spawn" />
+            <TextInput
+              {...field}
+              label="label"
+              placeholder="Spawn"
+              error={errors.label?.message}
+            />
           )}
         />
         <Controller
           name="x"
           control={control}
-          render={({ field }) => <NumberInput {...field} label="x" />}
+          render={({ field }) => (
+            <NumberInput {...field} label="x" error={errors.x?.message} />
+          )}
         />
         <Controller
           name="y"
           control={control}
-          render={({ field }) => <NumberInput {...field} label="y" />}
+          render={({ field }) => (
+            <NumberInput {...field} label="y" error={errors.y?.message} />
+          )}
         />
         <Controller
           name="z"
           control={control}
-          render={({ field }) => <NumberInput {...field} label="z" />}
+          render={({ field }) => (
+            <NumberInput {...field} label="z" error={errors.z?.message} />
+          )}
         />
         <Button fullWidth type="submit" mt="lg">
           Add Point
