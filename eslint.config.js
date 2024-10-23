5ae367bbb8cec74290b51d187f6ed29ab29ba14a
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-10-23 18:42:45 -0400

    update eslint config to ignore from gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,29 @@
-const nx = require('@nx/eslint-plugin');
+const nx = require("@nx/eslint-plugin")
+const { includeIgnoreFile } = require("@eslint/compat")
+const path = require("path")
+
+const gitignorePath = path.resolve(__dirname, ".gitignore")
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
+  includeIgnoreFile(gitignorePath),
+  ...nx.configs["flat/base"],
+  ...nx.configs["flat/typescript"],
+  ...nx.configs["flat/javascript"],
   {
-    ignores: ['**/dist'],
+    ignores: ["**/dist"],
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
+      "@nx/enforce-module-boundaries": [
+        "error",
         {
           enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          allow: ["^.*/eslint(\\.base)?\\.config\\.[cm]?js$"],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: "*",
+              onlyDependOnLibsWithTags: ["*"],
             },
           ],
         },
@@ -26,8 +31,8 @@ module.exports = [
     },
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     // Override or add rules here
     rules: {},
   },
-];
+]
