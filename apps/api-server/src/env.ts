39aa8e5962b8d19626a8799076bbf53113a0c4ab
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2025-02-10 01:38:22 +0700

    fix cors issue

--- apps/api-server/src/env.ts
+++ apps/api-server/src/env.ts
@@ -2,6 +2,6 @@ import "dotenv/config"
 export const PORT = process.env.PORT || 5500
 export const BASE_URL = process.env.BASE_URL || "http://localhost:5500"
 export const DATABASE_URL = process.env.DATABASE_URL
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:8081"
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:8082"
 export const SUPER_TOKENS_CONNECTION_URI =
   process.env.SUPER_TOKENS_CONNECTION_URI || "http://localhost:3567"
